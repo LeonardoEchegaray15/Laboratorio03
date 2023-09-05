2ed8c2148807c9192277439dcc8a5041edb15e25
LeonardoEchegaray15 <105308866+LeonardoEchegaray15@users.noreply.github.com>
2023-09-04 23:07:07 -0500

    Add files via upload

---
+++ src/laboratorio03/Persona.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio03;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Persona {
+    private String Nombre;
+    private String Apellido;
+    private String DNI;
+
+    public Persona(String Nombre, String Apellido, String DNI) {
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.DNI = DNI;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getDNI() {
+        return DNI;
+    }
+
+    public void setDNI(String DNI) {
+        this.DNI = DNI;
+    }
+    
+}
